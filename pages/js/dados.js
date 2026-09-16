dc45386335071fd21cde84d4c63f2a7de10e7797
iBm <raquelpiedra506@gmail.com>
2026-09-15 21:54:46 -0300

    Organização do javascript

--- pages/js/dados.js
+++ pages/js/dados.js
@@ -1,147 +1,183 @@
 const dados = [
 
     {
+        id: 1,
         data: "2026-09-01",
         hora: "10:45",
         preco: "R$ 30,00",
         "evento-novo": "Musicais",
-        imagem: "assents/img/musicais.png"
+        imagem: "assents/img/musicais.png",
+        vaga: 150
     },
 
     {
+        id: 2,
         data: "2026-09-05",
         hora: "16:00",
         preco: "R$ 45,00",
         "evento-novo": "Festival de Música",
-        imagem: "assents/img/festival.jpg"
+        imagem: "assents/img/festival.jpg",
+        vaga: 220
     },
 
     {
+        id: 3,
         data: "2026-09-12",
         hora: "14:30",
         preco: "R$ 50,00",
         "evento-novo": "Teatro",
-        imagem: "assents/img/teatro.jpg"
+        imagem: "assents/img/teatro.jpg",
+        vaga: 135
     },
 
     {
+        id: 4,
         data: "2026-09-17",
         hora: "19:00",
         preco: "R$ 70,00",
         "evento-novo": "Cinema",
-        imagem: "assents/img/cinema.jpg"
+        imagem: "assents/img/cinema.jpg",
+        vaga: 120
     },
 
     {
+        id: 5,
         data: "2026-09-23",
         hora: "20:00",
         preco: "R$ 90,00",
         "evento-novo": "Stand-up",
-        imagem: "assents/img/standup.jpg"
+        imagem: "assents/img/standup.jpg",
+        vaga: 140
     },
 
     {
+        id: 6,
         data: "2026-09-27",
         hora: "18:30",
         preco: "R$ 35,00",
         "evento-novo": "Feira Cultural",
-        imagem: "assents/img/feira.jpg"
+        imagem: "assents/img/feira.jpg",
+        vaga: 160
     },
 
     {
+        id: 7,
         data: "2026-10-03",
         hora: "15:00",
         preco: "R$ 25,00",
         "evento-novo": "Exposição de Arte",
-        imagem: "assents/img/exposicao.jpg"
+        imagem: "assents/img/exposicao.jpg",
+        vaga: 120
     },
 
     {
+        id: 8,
         data: "2026-10-08",
         hora: "19:30",
         preco: "R$ 80,00",
         "evento-novo": "Show ao Vivo",
-        imagem: "assents/img/show.jpg"
+        imagem: "assents/img/show.jpg",
+        vaga: "Ilimitada"
     },
 
     {
+        id: 9,
         data: "2026-10-11",
         hora: "14:00",
         preco: "R$ 40,00",
         "evento-novo": "Festival Gastronômico",
-        imagem: "assents/img/gastronomia.jpg"
+        imagem: "assents/img/gastronomia.jpg",
+        vaga: 125
     },
 
     {
+        id: 10,
         data: "2026-10-16",
         hora: "20:00",
         preco: "R$ 60,00",
         "evento-novo": "Dança",
-        imagem: "assents/img/danca.jpg"
+        imagem: "assents/img/danca.jpg",
+        vaga: 132
     },
 
     {
+        id: 11,
         data: "2026-10-21",
         hora: "19:00",
         preco: "R$ 55,00",
         "evento-novo": "Palestra",
-        imagem: "assents/img/palestra.jpg"
+        imagem: "assents/img/palestra.jpg",
+        vaga: 170
     },
 
     {
+        id: 12,
         data: "2026-10-25",
         hora: "17:30",
         preco: "R$ 75,00",
         "evento-novo": "Festival de Cinema",
-        imagem: "assents/img/festival-cinema.jpg"
+        imagem: "assents/img/festival-cinema.jpg",
+        vaga: 165
     },
 
     {
+        id: 13,
         data: "2026-11-02",
         hora: "10:00",
         preco: "R$ 20,00",
         "evento-novo": "Feira de Artesanato",
-        imagem: "assents/img/artesanato.jpg"
+        imagem: "assents/img/artesanato.jpg",
+        vaga: 220
     },
 
     {
+        id: 14,
         data: "2026-11-07",
         hora: "19:30",
         preco: "R$ 100,00",
         "evento-novo": "Show Nacional",
-        imagem: "assents/img/show-nacional.jpg"
+        imagem: "assents/img/show-nacional.jpg",
+        vaga: 300
     },
 
     {
+        id: 15,
         data: "2026-11-12",
         hora: "18:00",
         preco: "R$ 65,00",
         "evento-novo": "Teatro Musical",
-        imagem: "assents/img/teatro-musical.jpg"
+        imagem: "assents/img/teatro-musical.jpg",
+        vaga: 130
     },
 
     {
+        id: 16,
         data: "2026-11-15",
         hora: "16:30",
         preco: "R$ 35,00",
         "evento-novo": "Evento Geek",
-        imagem: "assents/img/geek.jpg"
+        imagem: "assents/img/geek.jpg",
+        vaga: 175
     },
 
     {
+        id: 17,
         data: "2026-11-20",
         hora: "20:00",
         preco: "R$ 85,00",
         "evento-novo": "Comédia",
-        imagem: "assents/img/comedia.jpg"
+        imagem: "assents/img/comedia.jpg",
+        vaga: 150
     },
 
     {
+        id: 18,
         data: "2026-11-28",
         hora: "15:00",
         preco: "R$ 30,00",
         "evento-novo": "Feira Cultural",
-        imagem: "assents/img/feira-cultural.jpg"
+        imagem: "assents/img/feira-cultural.jpg",
+        vaga: "Livre ao público."
     }
 
 ];
