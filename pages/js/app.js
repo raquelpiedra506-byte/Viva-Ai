38d303cb2039b9fc9aca9a5e7d6375fc075c2347
iBm <raquelpiedra506@gmail.com>
2026-09-17 22:11:38 -0300

    Revert "Merge branch 'main' of https://github.com/raquelpiedra506-byte/Viva-Ai"
    This reverts commit a6c45eef32adb8cdc24432be18cf4b96f6fe59c1, reversing
    changes made to 9f77803d780c05b5096609a33e1329ed07f855ed.

--- pages/js/app.js
+++ pages/js/app.js
@@ -234,22 +234,6 @@ function pesquisar() {
     displayEventos(eventosFiltrados);
 }
 
-// CATEGORIAS DE EVENTOS
-
-const select = document.getElementById("categorias");
-
-select.addEventListener("change", function () {
-    const categoriaSelecionada = select.value.toLowerCase();
-
-    const eventosFiltrados = dados.filter(function (e) {
-        return e.categoria.toLowerCase() === categoriaSelecionada;
-    });
-
-    displayEventos(eventosFiltrados);
-
-});
-
-
 // BOTÃO PESQUISAR
 
 btnPesquisa.addEventListener("click", pesquisar);
